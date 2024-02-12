ae147052524dc51c13a6fbebfd569150847d2320
Kief <67562560+Kief5555@users.noreply.github.com>
2024-02-12 02:22:40 +0000

    More commits

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+//Packages
+
 const express = require("express");
 const app = express();
 const db = require('better-sqlite3')('auth.db', options);
-require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+const winston = require("winston");
 
 
+//Authentication Functions
+
+
+//Main
 app.get('/', (req, res) => {
     
 })
