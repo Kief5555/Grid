67a4690e12c04f5aeee4b3ce436101ffcc215eaa
Kief <67562560+Kief5555@users.noreply.github.com>
2024-02-12 03:12:03 +0000

    Functions

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/***
+ * File grid by printedwaste
+ * Host files that are accessible by authenticated users
+ */
+
+
+
 //Packages
 
 const express = require("express");
@@ -8,8 +15,41 @@ const cors = require("cors");
 const winston = require("winston");
 
 
-//Authentication Functions
+/**************Definitions
+ * auth table:
+ * id
+ * username (optional, unique)
+ * password (optional)
+ * email (optional, unique)
+ * profile (optional)
+ * key (optional)
+ * tempkey (optional)
+ * tempkeyexpiry (optional)
+ * 
+ * 
+ * Logic: Some users are admins, some are not. The ones that are not admin have a temp key that expires after a certain amount of time. 
+ */
 
+//Useful functions
+async function createTableIfNotExists() {
+    db.prepare('CREATE TABLE IF NOT EXISTS auth (id INTEGER PRIMARY KEY, username TEXT, password TEXT, email TEXT, profile TEXT, key TEXT, tempkey TEXT, tempkeyexpiry TEXT)').run();
+}
+//Authentication Functions
+async function createUser(username, password, email, profile) {
+    db.prepare('INSERT INTO auth (username, password, email, profile) VALUES (?, ?, ?, ?)').run(username, password, email, profile);
+}
+
+function createTempKey() {
+
+}
+
+function authorizeUser() {
+    
+}
+
+function deleteUser() {
+        
+}
 
 //Main
 app.get('/', (req, res) => {
