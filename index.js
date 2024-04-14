432746ff3ce6096f2d3c05cbe1b73ab565b8c6be
Kiefer <Kieferlin1001@gmail.com>
2024-04-14 09:28:31 -0700

    A

--- index.js
+++ index.js
@@ -163,6 +163,7 @@ app.get('/api/file/:id', async (req, res) => {
 app.post('/api/file/upload', authenticateUser, multer({ dest: 'files/', limits: { fileSize: 100000000 } }).single('file'), (req, res) => {
     const file = req.files.file;
     const { self, accessKey } = req.body;
+    console.log(req.body)
     if (!file) return res.status(400).send({ errors: ["File required"], success: false, data: null });
     const fileID = generateFileID()
     const filename = file.name;
